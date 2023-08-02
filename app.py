d070777bffe0b5c141020740b6d2b8c4a3fe5fa6
Vinh Phan <pvphan90@gmail.com>
2023-08-02 09:58:46 -0700

    updates

--- app.py
+++ app.py
@@ -25,31 +25,33 @@ polls_df = pd.read_csv("polls.csv").set_index("id")
 
 
 @app.route("/")
-def index():
+def index() -> str:
     return render_template("index.html", polls=polls_df)
 
 
 @app.route("/polls/<id>")
-def polls(id):
+def polls(id: str) -> str:
     poll = polls_df.loc[int(id)]
     print(poll)
     return render_template("show_poll.html", poll=poll)
 
 
 @app.route("/vote/<id>/<option>")
-def vote(id, option):
-    if request.cookies.get(f"vote_{id}_cookie") is None:
-        polls_df.at[int(id), "votes"+str(option)] += 1
+def vote(id: str, option: str) -> str:
+    cookieKey = f"vote_{id}_cookie"
+    cookie = request.cookies.get(cookieKey)
+    if cookie is None:
+        polls_df.at[int(id), "votes"+option] += 1
         polls_df.to_csv("polls.csv")
         response = make_response(redirect(url_for("polls", id=id)))
-        response.set_cookie(f"vote_{id}_cookie", str(option))
+        response.set_cookie(cookieKey, option)
         return response
     else:
         return "Cannot vote more than once! Go back <a href=" + url_for("polls", id=id) + ">here</a>"
 
 
 @app.route("/polls", methods=["GET", "POST"])
-def create_poll():
+def create_poll() -> str:
     if request.method == "GET":
         return render_template("new_poll.html")
     elif request.method == "POST":
@@ -61,6 +63,8 @@ def create_poll():
         polls_df.loc[max(polls_df.index.values) + 1] = [poll, option1, option2, option3, option4, 0, 0, 0, 0]
         polls_df.to_csv("polls.csv")
         return redirect(url_for("index"))
+    else:
+        return ""
 
 
 if __name__ == "__main__":
